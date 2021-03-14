b5667b563fafd70efde4ddc8b464f91b7aed6ef2
vaneks <35092751+vaneks@users.noreply.github.com>
2021-03-14 15:45:05 +0800

    Update JavaIOSkillRepositoryImpl.java

--- src/main/java/com/vaneks/crud/repository/io/JavaIOSkillRepositoryImpl.java
+++ src/main/java/com/vaneks/crud/repository/io/JavaIOSkillRepositoryImpl.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class JavaIOSkillRepositoryImpl implements com.vaneks.crud.repository.SkillRepository {
 
-    private final String file = getClass().getClassLoader().getResource("/skills.txt").getFile();
+    private final String file = "skills.txt";
     public List<Skill> getAll() {
         return getAllInternal();
     }
